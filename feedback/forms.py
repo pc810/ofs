d208a82831dbe3706f1a4cadcc7a96183ef1011e
priyank chaudhari <priyank810123@gmail.com>
2019-03-05 01:01:29 +0530

    Added Form Form

---
+++ feedback/forms.py
@@ -0,0 +1,26 @@
+from .models import Form, Question
+from django.contrib.auth.admin import User
+from django import forms
+from django.db import models
+
+
+#class FormsForm(forms.Form):
+    # FORM_STATUS = (
+    #     ('AC', 'active'),
+    #     ('DA', 'deactivated'),
+    #
+    # )
+    # form_heading = models.CharField(max_length=50)
+    # form_link = models.TextField()
+    # form_status = models.CharField(choices=FORM_STATUS, max_length=10)
+
+
+class NameForm(forms.Form):
+    FORM_STATUS = (
+        ('AC', 'active'),
+        ('DA', 'deactivated'),
+
+    )
+    form_heading = forms.CharField(max_length=50)
+    form_link = forms.CharField()
+    form_status = forms.ChoiceField(choices=FORM_STATUS)
